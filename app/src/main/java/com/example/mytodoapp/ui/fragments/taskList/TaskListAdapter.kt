9fcdabf1561b63b936723773c32ecf55f2e47252
behnawwm <behban20@gmail.com>
2021-05-08 11:17:21 +0430

    Adapter prep

---
+++ app/src/main/java/com/example/mytodoapp/ui/fragments/taskList/TaskListAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.mytodoapp.ui.fragments.taskList
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mytodoapp.databinding.ItemTaskBinding
+import com.example.mytodoapp.db.models.Task
+
+class TaskListAdapter : ListAdapter<Task, TaskListAdapter.TaskListViewHolder>(DiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskListViewHolder {
+        val binding = ItemTaskBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TaskListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TaskListViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        holder.bind(currentItem)
+    }
+
+    class TaskListViewHolder(private val binding: ItemTaskBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(task: Task) {
+            binding.apply {
+                checkBoxDone.isChecked = task.done
+                checkBoxImportant.isChecked = task.starred
+                tvTitle.text = task.title
+                tvDate.text = task.createdDateFormatted
+
+                //crossed
+                tvDate.paint.isStrikeThruText = task.done
+                tvTitle.paint.isStrikeThruText = task.done
+
+            }
+        }
+    }
+
+    class DiffCallback : DiffUtil.ItemCallback<Task>() {
+        override fun areItemsTheSame(oldItem: Task, newItem: Task) =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: Task, newItem: Task) =
+            oldItem == newItem
+    }
+}
--- app/src/main/java/com/example/mytodoapp/ui/fragments/taskList/TaskListFragment.kt
+++ app/src/main/java/com/example/mytodoapp/ui/fragments/taskList/TaskListFragment.kt
@@ -1,13 +1,36 @@
 package com.example.mytodoapp.ui.fragments.taskList
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.asLiveData
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mytodoapp.R
+import com.example.mytodoapp.databinding.FragmentTaskListBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TaskListFragment : Fragment(R.layout.fragment_task_list) {
     private val viewModel: TaskListViewModel by viewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentTaskListBinding.bind(view)
+
+        val taskAdapter = TaskListAdapter()
+
+        binding.apply {
+            recyclerViewTasks.apply {
+                adapter = taskAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+        }
+
+        viewModel.getAllTasks().asLiveData().observe(viewLifecycleOwner) {
+            taskAdapter.submitList(it)
+        }
+    }
 
 }
